3053e7435d1173846ee008bed5e9d1c31bbe86a5
Alexandru Florea <alexflorea16000@gmail.com>
2021-12-03 20:15:56 +0200

    Kafka setup

--- src/main/java/org/pcbe/Main.java
+++ src/main/java/org/pcbe/Main.java
@@ -1,9 +1,30 @@
 package org.pcbe;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Properties;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello world!");
+        Properties props = new Properties();
+        props.setProperty("bootstrap.servers", "localhost:9092");
+        props.setProperty("group.id", "test");
+        props.setProperty("enable.auto.commit", "true");
+        props.setProperty("auto.commit.interval.ms", "1000");
+        props.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.setProperty("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(List.of("plutus"));
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+            for (ConsumerRecord<String, String> record : records)
+                System.out.printf("key = %s, value = %s%n", record.key(), record.value());
+        }
     }
 
 }
